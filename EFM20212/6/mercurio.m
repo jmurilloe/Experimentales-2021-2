681a688d719c5b0c412deddc0aa156c5ef929cc3
jmurilloe <70216386+jmurilloe@users.noreply.github.com>
2021-12-12 17:05:16 -0500

    Se finaliza el analisis de mercurio para practica 6 EFM.

--- EFM20212/6/mercurio.m
+++ EFM20212/6/mercurio.m
@@ -1,6 +1,6 @@
 clear all; close all;
 y1 = 27.0; y2 = 29.5; y3 = 33.0; y4 = 37.5; y5 = 39.5; deltay = 0.5;
-D = 131; d = 2e-6; m = 1;
+D = 131; d = 2e-6; m = 1; q = 2;
 theta1 = atan(y1/D)
 deltatheta1 = deltay*(1/(D+y1))
 theta2 = atan(y2/D)
@@ -21,3 +21,17 @@ lambda4 = d*sin(theta4)/m
 deltalambda4 = deltatheta4*(d/m)*cos(theta4)
 lambda5 = d*sin(theta5)/m
 deltalambda5 = deltatheta5*(d/m)*cos(theta5)
+for i = 3:7
+  x(i-2) = (1/q^2)-(1/i^2);
+end
+y = [1/lambda5 1/lambda4 1/lambda3 1/lambda2 1/lambda1];
+N = length(x); sxy = sum(x.*y); sxx = sum(x.*x); syy = sum(y.*y); sx = sum(x); sy = sum(y);
+a = (((N)*(sxy))-((sx)*(sy)))/(((N)*(sxx))-((sx)*(sx)))
+b = (((sxx)*(sy))-((sx)*(sxy)))/(((N)*(sxx))-((sx)*(sx)))
+e = (y-((a*x)+b)).^2; sei = sum(e);
+incertidumbrea=sqrt((sei)/(N-2))*sqrt((N)/(((N)*(sxx))-((sx)*(sx))))
+incertidumbreb=sqrt((sei)/(N-2))*sqrt((sxx)/(((N)*(sxx))-((sx)*(sx))))
+r = (((N)*(sxy))-((sx)*(sy)))/(sqrt(((N)*(sxx))-((sx)*(sx)))*sqrt(((N)*(syy))-((sy)*(sy))))
+xx = min(x):0.01:max(x); yy = a*xx+b;
+plot(x,y,'k*',xx,yy,'r-'); legend('Datos Experimentales','Ajuste lineal por MC', 'Location','southeast')
+xlabel('(1/2)^2-(1/p)^2');ylabel('1/\lambda[m^{-1}]');
