d8d64195375e87557511c88a697549e88eb12b4e
jmurilloe <70216386+jmurilloe@users.noreply.github.com>
2021-12-17 15:17:02 -0500

    Se terminan codigos para todo lo relacionado con Diodos para EA. Puntualmente la ultima modificacion es un ajuste mediante MC

--- Electronica Analogica/Entrega4/Factor de Rizado/Capacitancia Variable/capacitancia.m
+++ Electronica Analogica/Entrega4/Factor de Rizado/Capacitancia Variable/capacitancia.m
@@ -8,4 +8,23 @@ DELTAVP = [0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 ];
 VCD = VP.*(1-((1)./(2*pi*F*RL*C)));
 DELTAVCD = DELTAVP.*(1-((1)./(2*pi*F*RL*C)));
 FR = (VR./VCD)*100;
-plot(C,FR,'-*k')
+
+%%%%% Ajuste escala log mediante Minimos Cuadrados
+xp = C(3:length(C)); yp = FR(3:length(FR)); x = log(xp); y = log(yp); N = length(x)
+sxy = sum(x.*y); sxx = sum(x.*x); syy = sum(y.*y); sx = sum(x); sy = sum(y);
+a = (((N)*(sxy))-((sx)*(sy)))/(((N)*(sxx))-((sx)*(sx)))
+b = (((sxx)*(sy))-((sx)*(sxy)))/(((N)*(sxx))-((sx)*(sx)))
+e = (y-((a*x)+b)).^2; sei = sum(e);
+incertidumbrea=sqrt((sei)/(N-2))*sqrt((N)/(((N)*(sxx))-((sx)*(sx))))
+incertidumbreb=sqrt((sei)/(N-2))*sqrt((sxx)/(((N)*(sxx))-((sx)*(sx))))
+r = (((N)*(sxy))-((sx)*(sy)))/(sqrt(((N)*(sxx))-((sx)*(sx)))*sqrt(((N)*(syy))-((sy)*(sy))))
+
+%%%%% recta ajuste
+xx = min(x):0.01:max(x); yy = a*xx+b;
+
+%%%%% FIGURAS. 1. Interpolacion datos experimentales. 2. Ajuste lineal en escala log
+figure(1); clf;
+plot(C,FR,'*-k')
+
+figure(2); clf;
+plot(x,y,'k*',xx,yy,'-r')
--- Electronica Analogica/Entrega4/Factor de Rizado/Resistencia Variable/resistencia.m
+++ Electronica Analogica/Entrega4/Factor de Rizado/Resistencia Variable/resistencia.m
@@ -1,4 +1,5 @@
 clear all; close all;
+%%%%% Calculos para factor de rizado en funcion de la resistencia
 C = 100e-6; F = 60;
 RL = [50 100 200 300 500 800 1000 2000 3000 5000 10000];
 VR = [4.6 3.2 2 1.5 1.0 0.68 0.56 0.3 0.22 0.14 0.09];
@@ -8,4 +9,23 @@ DELTAVP = [0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 ];
 VCD = VP.*(1-((1)./(2*pi*F*RL*C)));
 DELTAVCD = DELTAVP.*(1-((1)./(2*pi*F*RL*C)));
 FR = (VR./VCD)*100;
-plot(RL,FR,'-*k')
+
+%%%%% Ajuste escala log mediante Minimos Cuadrados
+x = log(RL); y = log(FR); N = length(x)
+sxy = sum(x.*y); sxx = sum(x.*x); syy = sum(y.*y); sx = sum(x); sy = sum(y);
+a = (((N)*(sxy))-((sx)*(sy)))/(((N)*(sxx))-((sx)*(sx)))
+b = (((sxx)*(sy))-((sx)*(sxy)))/(((N)*(sxx))-((sx)*(sx)))
+e = (y-((a*x)+b)).^2; sei = sum(e);
+incertidumbrea=sqrt((sei)/(N-2))*sqrt((N)/(((N)*(sxx))-((sx)*(sx))))
+incertidumbreb=sqrt((sei)/(N-2))*sqrt((sxx)/(((N)*(sxx))-((sx)*(sx))))
+r = (((N)*(sxy))-((sx)*(sy)))/(sqrt(((N)*(sxx))-((sx)*(sx)))*sqrt(((N)*(syy))-((sy)*(sy))))
+
+%%%%% recta ajuste
+xx = min(x):0.01:max(x); yy = a*xx+b;
+
+%%%%% FIGURAS. 1. Interpolacion datos experimentales. 2. Ajuste lineal en escala log
+figure(1); clf;
+plot(RL,FR,'*k')
+
+figure(2); clf;
+plot(x,y,'k*',xx,yy,'-r')
